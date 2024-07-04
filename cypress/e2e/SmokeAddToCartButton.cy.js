5d5872bbfd9b9f0244b9878bb10c315165c52226
aymericGodivier <44438326+aymericGodivier@users.noreply.github.com>
2024-07-04 15:32:06 +0200

    chnages

--- cypress/e2e/SmokeAddToCartButton.cy.js
+++ cypress/e2e/SmokeAddToCartButton.cy.js
@@ -6,7 +6,7 @@ describe('Smoke Test for the presence of Add to cart button before and after log
       // Visitez la page d'un produit
       cy.visit(`${baseUrl}/#/products/${productID}`);  
       //Vérifie la présence du bouton ajouetr au panier
-      cy.get('[data-cy="detail-product-add"]').should('be.visible');//on ne devrait pas trouver le bouton si l'on n'est pas connecté
+      cy.get('[data-cy="detail-product-add"]').should.not('be.visible');//on ne devrait pas trouver le bouton si l'on n'est pas connecté
     });
 
     it('should confirm the presence of the Ajouter au panier button after login', () => {
